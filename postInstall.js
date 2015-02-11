e4cf6fdeef9bb01ce68e64889081da10d0ca6c13
Jamy Timmermans <me@jamy.be>
2015-02-11 14:22:52 +0100

    log error

--- postInstall.js
+++ postInstall.js
@@ -1,7 +1,9 @@
 var exec = require("child_process").exec;
 
 if (process.platform !== 'win32') {
-    exec('npm install usage', function () {
-        //
+    exec('npm install usage', function (err, stdout, stderr) {
+        if (err) {
+        	console.log(err, stdout, stderr);
+        }
     });
 }
